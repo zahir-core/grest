55575585a712b0fa646e03a9e5e863a81a5ab665
Jeffry Luqman <jeffry.luqman@gmail.com>
2022-04-01 07:22:39 +0700

    NewUUIDSQL

--- db/query.go
+++ db/query.go
@@ -753,3 +753,20 @@ func QuoteJSON(db *gorm.DB, column, jsonKey string) string {
 		return db.Statement.Quote(column)
 	}
 }
+
+func NewUUIDSQL(db *gorm.DB) string {
+	switch db.Dialector.Name() {
+	case "sqlite":
+		return "lower(hex(randomblob(4))) || '-' || lower(hex(randomblob(2))) || '-4' || substr(lower(hex(randomblob(2))),2) || '-' || substr('89ab',abs(random()) % 4 + 1, 1) || substr(lower(hex(randomblob(2))),2) || '-' || lower(hex(randomblob(6)))"
+	case "mysql":
+		return "UUID()"
+	case "postgres":
+		return "md5(random()::text || clock_timestamp()::text)::uuid"
+	case "sqlserver":
+		return "LOWER(CAST(NEWID() AS CHAR(36)))"
+	case "firebird":
+		return "LOWER(UUID_TO_CHAR(GEN_UUID()))"
+	default:
+		return ""
+	}
+}
